dd6efd586b0f0ef8c4dbed96c1a51a44ed53411c
KorbinWeiler <korbinweiler@gmail.com>
2025-05-28 23:30:17 -0700

    Started implementing client server

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
 import { createContext } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import GameBoard from './components/GameBoard'
 import GameBoardUtils from './Utils/GameBoardUtils'
+
+const express = require('express')
+
+const clientServer = express()
 
 export const UpdateContext = createContext(null)
 let test = new GameBoardUtils();
